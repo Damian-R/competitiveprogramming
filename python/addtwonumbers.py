82748d4d79be72d0341dbd44ca7f03d7f89c5d00
Damian Reiter <damian.reiter@gmail.com>
2018-08-13 16:42:07 -0400

    #2: Add Two Numbers

--- python/addtwonumbers.py
+++ python/addtwonumbers.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def addTwoNumbers(self, l1, l2):
@@ -27,28 +27,3 @@ class Solution:
                 travel = travel.next
             total = total // 10
         return head
-
-        
-        
-
-sol = Solution()
-
-l1 = ListNode(2)
-l2 = ListNode(4)
-l3 = ListNode(3)
-l1.next = l2
-l2.next = l3
-
-l4 = ListNode(5)
-l5 = ListNode(6)
-l6 = ListNode(4)
-l4.next = l5
-l5.next = l6
-# l1 = ListNode(5)
-# l4 = ListNode(5)
-
-res = sol.addTwoNumbers(l1, l4)
-
-while res:
-    print (res.val)
-    res = res.next
