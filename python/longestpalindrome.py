f3e448b70fee92038de006e9469a293e0136b7e8
Damian Reiter <damian.reiter@gmail.com>
2018-08-14 19:19:54 -0400

    5: Longest Palindrome Substring

---
+++ python/longestpalindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        maxlength, string = 0, ''
+        for i in range(2*len(s) - 1):
+            start, double = (i, False) if i < len(s) else (i - len(s), True)
+            x, length = 0, 0
+            startind, endind = (start - x, start + x) if not double else (start, start + 1)
+            while (startind > -1) and (endind < len(s)) and (s[startind] == s[endind]):
+                if startind == endind:
+                    length -= 1
+                length += 2
+                if length > maxlength:
+                    maxlength = length
+                    string = s[startind:endind+1]
+                startind -= 1
+                endind += 1
+        return string
+
+print("res: " + Solution().longestPalindrome('a'))
