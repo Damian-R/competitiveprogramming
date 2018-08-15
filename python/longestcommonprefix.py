724b29f124452a08d4df0ef4e2d8b8d2ce813097
Damian Reiter <damian.reiter@gmail.com>
2018-08-15 00:14:42 -0400

    14. Longest Common Prefix

---
+++ python/longestcommonprefix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """        
+        if len(strs) == 0:
+            return ''
+        prefix = ''
+        for i in range(min(list(map(lambda e: len(e), strs)))):
+            for j, e in enumerate(strs[:-1]):
+                if strs[j][i] is not strs[j+1][i]:
+                    return prefix
+            prefix += strs[0][i]
+        return prefix
+
+
+print(Solution().longestCommonPrefix(["flower","flow","flight"]))
+# print(Solution().longestCommonPrefix([""]))
