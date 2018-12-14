2cdc96c43bbbe6ba20054bc7fbff213817352a76
Damian Reiter <damian.reiter@gmail.com>
2018-12-13 23:35:57 -0500

    1.5: One Away

---
+++ CTCI/chapter 1/one_away.py
@@ -0,0 +1,21 @@
+def one_away(one, two):
+    if one == two: return True
+    diff = 0
+    if len(one) == len(two):
+        for i, e in enumerate(one):
+            if e != two[i]:
+                if diff == 1: return False
+                diff = 1
+    elif len(one) != len(two):
+        longer = one if len(one) > len(two) else two
+        shorter = one if len(one) < len(two) else two
+        for i, e in enumerate(longer):
+            if i == len(shorter) and diff == 0: return True
+            if e != shorter[i - diff]:
+                if diff == 1: return False
+                diff = 1
+    return True
+
+
+
+print one_away('pale', 'pales')
