935db6b59afa5206351474144c977d3325c4b067
Damian Reiter <damian.reiter@gmail.com>
2018-12-30 12:16:22 -0500

    2.2 Return Kth to Last

---
+++ CTCI/chapter 2/java/returnKthToLast.java
@@ -0,0 +1,28 @@
+import helpers.SinglyLinkedList;
+import helpers.Node;
+
+class returnKthToLast {
+  public static void main(String[] args) {
+    SinglyLinkedList list = new SinglyLinkedList(new Node(5)); // fifth to last
+    list.addNode(new Node(4)); // fourth to last
+    list.addNode(new Node(5)); // third to last
+    list.addNode(new Node(2)); // second to last
+    list.addNode(new Node(6)); // first to last
+    Node kthToLast = returnKthToLast(list, 2);
+    System.out.println(kthToLast.d);
+  }
+
+  static Node returnKthToLast(SinglyLinkedList list, int k) {
+    Node travel = list.head;
+    Node follow = list.head;
+
+    for (int i = 0; i < k-1; i++) travel = travel.next;
+
+    while (travel.next != null) {
+      travel = travel.next;
+      follow = follow.next;
+    }
+
+    return follow;
+  }
+}
