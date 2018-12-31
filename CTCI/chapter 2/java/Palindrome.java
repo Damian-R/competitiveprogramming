8ed2f729ee116065e8ec2a9ff8fc5e313a7827a1
Damian Reiter <damian.reiter@gmail.com>
2018-12-31 14:50:11 -0500

    rewrite using better algo

--- CTCI/chapter 2/java/Palindrome.java
+++ CTCI/chapter 2/java/Palindrome.java
@@ -1,38 +1,44 @@
 import helpers.Node;
 import helpers.SinglyLinkedList;
+import java.util.Stack;
 
 class Palindrome {
   public static void main(String[] args) {
     SinglyLinkedList list = new SinglyLinkedList();
     list.addNode(new Node(3));
-    list.addNode(new Node(5));
     list.addNode(new Node(4));
-    list.addNode(new Node(5));
+    list.addNode(new Node(6));
+    list.addNode(new Node(6));
+    list.addNode(new Node(4));
     list.addNode(new Node(3));
     boolean result = palindrome(list);
     System.out.println(result);
   }
 
   static boolean palindrome(SinglyLinkedList list) {
-    Node reverseHead = new Node(list.head.d);
-    Node travel1 = list.head.next;
-    while (travel1 != null) {
-      Node n = new Node(travel1.d);
-      n.next = reverseHead;
-      reverseHead = n;
-      travel1 = travel1.next;
+    Stack<Integer> stack = new Stack<Integer>();
+
+    Node fast = list.head;
+    Node slow = list.head;
+
+    while (fast != null && fast.next != null) {
+      stack.push(slow.d);
+      slow = slow.next;
+      fast = fast.next.next;
     }
 
-    travel1 = list.head;
-    Node travel2 = reverseHead;
-
-    while (travel1 != null) {
-      if (travel1.d != travel2.d) return false;
-      travel1 = travel1.next;
-      travel2 = travel2.next;
+    if (fast != null && fast.next == null) {
+      slow = slow.next;
     }
-    return true;
+
+    while (slow != null) {
+      if (slow.d != stack.pop()) return false;
+      slow = slow.next;
+    }
+
+    return stack.isEmpty();
   }
 }
 
 // 3 5 4 5 3
+// 3 5 4 4 5 3
